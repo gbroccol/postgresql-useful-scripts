c621d9f3659b8551b72123cf84e3b770ef98a10a
Пчелина Анастасия <pchelina.a2@wildberries.work>
2024-02-09 21:56:11 +0100

    Add scripts

---
+++ structure/related-with-schemas/scripts.sql
@@ -0,0 +1,8 @@
+-----------------------------------------------
+-- RELATED WITH SCHEMAS
+-----------------------------------------------
+
+-- To list schemas names
+SELECT n.nspname
+FROM pg_namespace n
+ORDER BY n.nspname;
